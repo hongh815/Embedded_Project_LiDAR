6d4349084f8365f11942fd9d925a7d767292e566
hongh815 <hongh815@gmail.com>
2020-11-12 12:52:03 +0900

    LE

---
+++ CMmakeFILES/ydlidar_client_WiringPI_in.cpp
@@ -0,0 +1,59 @@
+/*
+ *  YDLIDAR SYSTEM
+ *  YDLIDAR ROS Node Client 
+ *
+ *  Copyright 2015 - 2018 EAI TEAM
+ *  http://www.ydlidar.com
+ * 
+ */
+
+#include "ros/ros.h"
+#include "sensor_msgs/LaserScan.h"
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <wiringPi.h>
+#include <wiringSerial.h>
+
+
+#define RAD2DEG(x) ((x)*180./M_PI)
+
+char device[] = "/dev/ttyACM0";
+int fd;
+unsigned long baud = 9600;
+
+void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
+{
+    int count = scan->scan_time / scan->time_increment;
+    printf("[YDLIDAR INFO]: I heard a laser scan %s[%d]:\n", scan->header.frame_id.c_str(), count);
+    printf("[YDLIDAR INFO]: angle_range : [%f, %f]\n", RAD2DEG(scan->angle_min), RAD2DEG(scan->angle_max));
+  
+    for(int i = 0; i < count; i++) {
+        float degree = RAD2DEG(scan->angle_min + scan->angle_increment * i);
+	if(degree > -5 && degree< 5) //dgree data print
+        printf("[YDLIDAR INFO]: angle-distance : [%f, %f, %i]\n", degree, scan->ranges[i], i);
+        serialPutchar(fd, 65);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    if((fd = serialOpen (device, baud)) < 0) {
+        fprintf(stderr, "Uable to open serial device : %s\n", strerror(errno));
+        exit(1);
+    }
+
+    if(wiringPiSetup() == -1) {
+        fprintf(stderr, "Unable to start wiringPi : %s\n", strerror(errno));
+        exit(1);
+    }
+
+    ros::init(argc, argv, "ydlidar_client");
+    ros::NodeHandle n;
+    ros::Subscriber sub = n.subscribe<sensor_msgs::LaserScan>("/scan", 1000, scanCallback);
+    ros::spin();
+
+    return 0;
+}
